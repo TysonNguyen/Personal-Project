4c1f2f638b1ad12eb7e1b5188c37b593e43eb986
TysonNguyen <phongtong2908@gmail.com>
2024-03-15 10:04:16 -0600

    Code Tester

---
+++ Code Testing/query.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+  const Labels = document.querySelectorAll(".myButton");
+  for (var i = 0; i < Labels.length; i++) {
+    Labels[i].addEventListener("mouseenter", Enter);
+    Labels[i].addEventListener("mouseleave", Whiten);
+  }
+
+  const Buttons = document.querySelectorAll("#myButton");
+  for (var j = 0; j < Buttons.length; j++) {
+    Buttons[j].addEventListener("mouseenter", Clicked);
+  }
+};
+
+function Clicked() {
+  this.innerHTML = this.value;
+}
+function Whiten() {
+  document.body.style.backgroundColor = "white";
+}
+function Enter() {
+  document.body.style.backgroundColor = this.style.backgroundColor;
+}
