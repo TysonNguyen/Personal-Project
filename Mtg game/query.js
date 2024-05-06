18377644fd76bab5d5fc84bfca8e01770cac8a11
TysonNguyen <phongtong2908@gmail.com>
2024-05-06 00:34:59 -0600

    personal fille

--- Mtg game/query.js
+++ Mtg game/query.js
@@ -1,72 +1,43 @@
-var MTGurl = "https://api.scryfall.com/cards/search";
-var cardName,cardImg,cardMV,cardId;
-var strRequest;
-$(main);
-
-function main() {
-  $(`#search_btn`).click(btnClickRequest);
-  $(`#testAdd`).click(btnClickAdd);
+var setLogo = ["blb","mh3","otj","pip"]
+var MTGurl = "https://api.scryfall.com/cards/search"; 
+var loadMain = true;
+$(main) 
+function main(){
+    if(loadMain)
+    {
+        for(let i = 0; i < setLogo.length; i++)
+        {
+            const imgElement = document.createElement("img");
+            const aElement = document.createElement("a");
+            $(aElement).prop("href","./display/display.html")
+            $(imgElement).prop("src",`./image/${setLogo[i]}-logo.jfif`)
+            $(imgElement).val(setLogo[i]);
+            aElement.append(imgElement)
+            $(imgElement).click(IconClick)
+            $("#index_icon").append(aElement);
+        }        
+    }
 }
 
-function btnClickRequest() {
-   strRequest = BuildString();
-  let ob = {
-    as:"grid",
-    order: `name`,
-    q: strRequest
-  };
-  $.ajax({
-    url: MTGurl,
-    dataType: "json",
-    data: ob,
-    type: "GET",
-    success: SuccessAjax,
-    error: ErrorAjax,
-  });
-}
-function SuccessAjax(returnData, msg) {
-  $("#body-div").empty();
-  console.clear();
-  console.log(returnData);
-  for (let i = 0; i < returnData.total_cards; i++) {
-    let imgElement = document.createElement("img");
-    // obCardToJson.card.cardId = returnData.data[i].id
-    // obCardToJson.card.cardName = returnData.data[i].name
-    // obCardToJson.card.cardImg= returnData.data[i].image_uris.normal
-    // obCardToJson.card.cardMV = returnData.data[i].cmc
-    $(imgElement).prop("src", returnData.data[i].image_uris.normal);
-    $(imgElement).val(returnData.data[i].id);
-    $(imgElement).click(CardClick)
-    $("#body-div").append(imgElement);
-  }
+function IconClick(){
+    let ob = {
+        as: "grid",
+        order : 'set',
+        q: `set:${$(this).val()}`
+        
+    }
+    $.ajax({
+        url: MTGurl,
+        dataType: "json",
+        data: ob,
+        type: "GET",
+        success: SuccessCallSet,
+        error: () => {console.log("error")}
+    })
+    loadMain = false;
 }
 
-function ErrorAjax(xqh, msg) {
-
-  $("#body-div").empty();
-  $("#body-div").html(`There is no card with: ` + strRequest);
-}
-
-function CardClick(){
-  console.log($(this).val());
-}
-
-function btnClickAdd(){
-  $.ajax({
-    url: "cards.json",
-    dataType: "json",
-    data: obCardToJson,
-    type: "POST",
-
-  });
-}
-
-function BuildString(){
-  let strReturn = "";
-  if($("#cardMV").val() != "")
-  {
-    strReturn += `cmc=${Number($("#cardMV").val())} `
-  }
-  strReturn += `${$("#cardName").val()}`
-  return strReturn;
+function SuccessCallSet(data,mgs)
+{
+    console.log(data)
 }
