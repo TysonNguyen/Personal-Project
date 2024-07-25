66fa181016cc2b1642a69a038281ae37ec619aac
TysonNguyen <phongtong2908@gmail.com>
2024-07-25 16:24:30 -0600

    to do list

--- To do list/query.js
+++ To do list/query.js
@@ -1,13 +1,27 @@
 $(main);
 
-function main (){
-    $('#btn_Add').click(() => {
-        $('#txtBox_Input').val('');
-
-    })
+function main() {
+  $("#btn_Add").click(() => {
+    AddToDo($("#txtBox_Input").val());
+    $("#txtBox_Input").val("");
+  });
 }
 
-function AddToDo(name, time){
-    divElement = 
-    $('div_Display').html()
+function AddToDo(name, time) {
+  divElement = document.createElement("div");
+  $(divElement).html(name);
+  $(divElement).addClass("toDo");
+  $(divElement).dblclick(DeleteFunc);
+  $(divElement).click(CheckFunc);
+  $("#div_Display").append(divElement);
+}
+
+function CheckFunc() {
+  console.log($(this).html());
+  $(this).css("background-color","green");
+  console.log("Single Clicked");
+}
+function DeleteFunc() {
+  $(this).remove();
+  console.log("Double clicked");
 }
