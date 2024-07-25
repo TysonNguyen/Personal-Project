15d83d7424afc425858804a0b669aee1938ada63
TysonNguyen <phongtong2908@gmail.com>
2024-07-25 12:34:56 -0600

    comit

---
+++ To do list/query.js
@@ -0,0 +1,13 @@
+$(main);
+
+function main (){
+    $('#btn_Add').click(() => {
+        $('#txtBox_Input').val('');
+
+    })
+}
+
+function AddToDo(name, time){
+    divElement = 
+    $('div_Display').html()
+}
--- my-react-app/src/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-  return (
-    <footer>
-      <p>&copy; Tyson Nguyen {new Date().getFullYear()}</p>
-    </footer>
-  );
-}
-export default Footer;
